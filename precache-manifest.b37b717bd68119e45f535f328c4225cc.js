b468d1795303d369335ecd2fc8c8e87d37100c66
Manish Tuladhar <manish.eclipse@gmail.com>
2020-10-08 12:52:25 +0545

    Updates

---
+++ precache-manifest.b37b717bd68119e45f535f328c4225cc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a8cb9bdb7a2966f72bc1bec6e6bb04a",
+    "url": "/MonstersReactApp/index.html"
+  },
+  {
+    "revision": "00394a46b0453cb0f804",
+    "url": "/MonstersReactApp/static/css/main.fdec0984.chunk.css"
+  },
+  {
+    "revision": "70ac84b987b2b5a6fa3e",
+    "url": "/MonstersReactApp/static/js/2.164d1246.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/MonstersReactApp/static/js/2.164d1246.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00394a46b0453cb0f804",
+    "url": "/MonstersReactApp/static/js/main.68194c16.chunk.js"
+  },
+  {
+    "revision": "470eeadba76600673377",
+    "url": "/MonstersReactApp/static/js/runtime-main.47c3a7c8.js"
+  }
+]);
