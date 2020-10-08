b2d3a5d23a1fb320459072f45b0cb6f7a5e8399f
Manish Tuladhar <manish.eclipse@gmail.com>
2020-10-08 12:41:39 +0545

    Handling clicks and filters of the monsters

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import React, { Component } from "react";
 import "./App.css";
 import { CardList } from "./components/card-list/card-list.component";
+import { SearchBox } from "./components/search-box/search-box.component";
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      monsters: [
-        { name: "Frankenstein", id: "asc1" },
-        { name: "Dracula", id: "asc2" },
-        { name: "Zombie", id: "as3" },
-      ],
+      monsters: [],
+      searchField: "",
     };
   }
 
@@ -21,10 +19,23 @@ class App extends Component {
       .then((users) => this.setState({ monsters: users }));
   }
 
+  // handling filter change
+  handleChange = (e) => {
+    this.setState({ searchField: e.target.value });
+  };
+
   render() {
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter((monster) =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    );
     return (
       <div className="App">
-        <CardList monsters={this.state.monsters}></CardList>
+        <SearchBox
+          placeholder="Search Monsters"
+          handleChange={this.handleChange}
+        />
+        <CardList monsters={filteredMonsters} />
       </div>
     );
   }
